d6b93fa559f3b0c467f2e9d4a5410b38052f3a29
Muzammil Sajid <pc.21695.muzammil@gmail.com>
2024-06-02 11:54:31 +0500

    added new

---
+++ lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+//Custom class for Light & dark Text themes
+
+class TCheckboxTheme {
+  TCheckboxTheme._();
+  static CheckboxThemeData LightCheckTheme = CheckboxThemeData(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+      checkColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.white;
+        } else {
+          return Colors.black;
+        }
+      }),
+      fillColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.blue;
+        } else {
+          return Colors.transparent;
+        }
+      }));
+// customizable dark Text theme
+  static CheckboxThemeData darkCheckTheme = CheckboxThemeData(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+      checkColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.white;
+        } else {
+          return Colors.black;
+        }
+      }),
+      fillColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.blue;
+        } else {
+          return Colors.transparent;
+        }
+      }));
+}
+///custam
---
+++ lib/utils/theme/custom_themes/chip_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TChiptheme {
+  TChiptheme._();
+  // LightChiptheme
+  static ChipThemeData LightChipTheme = ChipThemeData(
+    disabledColor: Colors.grey.withOpacity(0.4),
+    labelStyle: const TextStyle(color: Colors.black),
+    selectedColor: Colors.blue,
+    padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+    checkmarkColor: Colors.white,
+  );
+  // darkchiptheme
+  static ChipThemeData darkChipTheme = ChipThemeData(
+    disabledColor: Colors.grey.withOpacity(0.4),
+    labelStyle: const TextStyle(color: Colors.black),
+    selectedColor: Colors.blue,
+    padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+    checkmarkColor: Colors.white,
+  );
+}
