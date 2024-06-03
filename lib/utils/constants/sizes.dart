64d9e85bb38947a31bcba786915e39834d26a84b
Muzammil Sajid <pc.21695.muzammil@gmail.com>
2024-06-03 23:43:07 +0500

    better updates

---
+++ lib/utils/constants/sizes.dart
@@ -0,0 +1,75 @@
+//import 'package:flutter/material.dart';
+class TSizes {
+
+  //padding and margin  size
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+
+  // Icons size
+  static const double iconxs = 12.0;
+  static const double iconSm = 16.0;
+  static const double iconMd = 24.0;
+  static const double iconLg = 32.0;
+
+  // font size
+  static const double fontSizeSm = 14.0;
+  static const double fontSizeMd = 16.0;
+  static const double fontSizeLg = 18.0;
+
+  // BUTTON size
+  static const double buttonHeight = 18.0;
+  static const double buttonRadius = 12.0;
+  static const double buttonWidth = 120.0;
+  static const double buttonElevation = 4.0;
+
+  // AppBar height
+  static const double appBarHeight = 56.0;
+
+  // Image size
+  static const double imageThumbSize = 80.0;
+
+  // default space between section
+  static const double defaultSpace = 24.0;
+  static const double defaultSpaceBtwItem = 16.0;
+  static const double defaultSpaceBtwSection = 32.0;
+
+  // border radius
+  static const double borderRadiusSm = 24.0;
+  static const double borderRadiusMd = 16.0;
+  static const double borderRadiusLg = 32.0;
+
+  //Divider height
+  static const double dividerHeight = 1.0;
+
+  //product items dimenstion
+  static const double productImageSize = 120.0;
+  static const double productImageRadius = 16.0;
+  static const double productImageHeight = 160.0;
+
+  //Input field
+  static const double inputfieldRadius = 12.0;
+  static const double spaceBtwFields = 16.0;
+
+  // card size
+  static const double cardRadiusLg = 16.0;
+  static const double cardRadiusMd = 12.0;
+  static const double cardRadiusSm = 10.0;
+  static const double cardRadiusXs = 6.0;
+  static const double cardElevation = 2.0;
+
+  // Image careousel height
+  static const double imageCarouselHeight = 200.0;
+
+  // Loading indicator size
+  static const double loadingIdicatorSize = 36.0;
+
+  // grid view spacing
+  static const double gridViewSpacing = 16.0;
+
+
+
+
+}
