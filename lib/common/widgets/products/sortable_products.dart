e1f97a4f83b6ebea5fdcc14d166aedaf55269496
Muzammil Sajid <pc.21695.muzammil@gmail.com>
2024-09-16 22:24:17 +0500

    dropdown sort

---
+++ lib/common/widgets/products/sortable_products.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_standard_ecommerce_app/common/widgets/layouts/grid_layout.dart';
+import 'package:flutter_standard_ecommerce_app/common/widgets/products/product_cards/product_card_vertical.dart';
+import 'package:flutter_standard_ecommerce_app/utils/constants/sizes.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TSortableProducts extends StatelessWidget {
+  const TSortableProducts({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //-- Dropdown
+        DropdownButtonFormField(
+          decoration: const InputDecoration(
+            prefixIcon: Icon(Iconsax.sort),
+          ),
+          onChanged: (value) {},
+          items: [
+            'Name', 'Higher Price', 'Lower Price', 'Sale', 'Newest', 'Popularity'
+          ]
+          .map((option) => DropdownMenuItem(
+            value: option, 
+            child: Text(option),
+          )) .toList(),
+        ),
+        const SizedBox(height: TSizes.defaultSpaceBtwSection),
+    
+        //-- Products
+        TGridLayout(
+          itemcount: 6,
+          itembuilder: (_, index) => const TProductCardVertical(),
+        ),
+      ],
+    );
+  }
+}
