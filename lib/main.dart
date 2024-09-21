700427787cc02435ad4823196c4dcc0d44f16430
Muzammil Sajid <pc.21695.muzammil@gmail.com>
2024-09-21 12:35:23 +0500

    Backend start integrate firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_standard_ecommerce_app/my_app.dart';
+import 'package:get/get.dart';
+import 'firebase_options.dart';
 
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
 
   //TODOS
@@ -10,7 +13,14 @@ void main() {
   //add widgets bindings
   //initialized loacal storage
   //await native splash
-  //initialize firebase
+
+//-- initialize firebase
+await Firebase.initializeApp( options: DefaultFirebaseOptions.currentPlatform).then(
+  //--- The AuthenticationRepository decide the state of the app either user loged in or loged out and manage to show which screen first
+  (FirebaseApp Value) => Get.put(AuthenticationRepository())
+);
+
+
   //initialize authentication
 }
 
